4fdc87913c0e35c6d2375bacc12a52da0326cbb2
amr abd elaaal <amrmohamed362363@gmail.com>
2021-12-06 23:57:06 +0200

    parsing XML string

--- src/Conversion.java
+++ src/Conversion.java
@@ -1,9 +1,41 @@
-
+import java.util.ArrayList;
 public class Conversion {
+
 
 	public Conversion() {
 		// TODO Auto-generated constructor stub
                 //hi
+	}
+	static void parsing_xml(String sa,ArrayList<String> string) {
+		char space=32;
+		for (int i = 0; i < sa.length(); i++) {
+			if (sa.charAt(i)==space||sa.charAt(i)=='\r'||sa.charAt(i)=='\n')continue;
+			
+			
+			else if (sa.charAt(i)=='<') {
+				
+				int j=i;
+				
+				while ((sa.charAt(i)!='>')) {
+					i++;
+				}
+				//if (sa.charAt(j+1)=='/')continue;
+				string.add(sa.substring(j,i+1));
+				
+				
+			}
+			else {
+				int k=i;
+				while((sa.charAt(i)!='<')) {
+					
+					if (sa.charAt(i)=='\r'||sa.charAt(i)=='\n')break;
+				i++;}
+				string.add(sa.substring(k,i));
+				--i;
+			}
+		}
+		
+			
 	}
 
 }
