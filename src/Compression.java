fa5d8085ea5afa4fa0cf8d398d237c8fd2811927
Amr Mohsen <amrmohsen2000@hotmail.com>
2021-12-09 00:03:44 +0200

    Added a hashtable that contains each character of the string to be encoded and its binary replacement code

--- src/Compression.java
+++ src/Compression.java
@@ -1,32 +1,49 @@
-import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Collections;
+
 public class Compression {
-	final static int NUMBER_CHAR = 256;
+	private final static int NUMBER_CHAR = 256;
+
+	//fill the hashtable which contains each character and its replacement
+	private static void fillReplacementTable(TreeNode root , String[] replacementTable , String code) {
+		if(root	instanceof Leaf) {
+			replacementTable[((Leaf)root).getCharacter()] = code;
+			return;
+		}
+		fillReplacementTable(root.leftChild ,replacementTable, code + "0");
+		fillReplacementTable(root.rightChild ,replacementTable, code + "1");
+	}
+	
+	
+
 	// where the magic happens
 	public static void encode(String s) {
 		int[] freqArray = new int[NUMBER_CHAR];
+		String[] replacementTable = new String[NUMBER_CHAR];
 		ArrayList<TreeNode> nodes = new ArrayList<TreeNode>();
+		String code = "";
 		TreeNode root;
-		for(int i = 0 ; i < s.length() ; i++) {
+		for (int i = 0; i < s.length(); i++) {
 			freqArray[s.charAt(i)]++;
 		}
-		for(int i = 0 ; i < NUMBER_CHAR ; i++) {
-			if(freqArray[i] != 0) {
-				nodes.add(new Leaf((char)i , freqArray[i]));
+		for (int i = 0; i < NUMBER_CHAR; i++) {
+			if (freqArray[i] != 0) {
+				nodes.add(new Leaf((char) i, freqArray[i]));
 			}
 		}
-		//Sort the nodes in ascending order to begin constructing the tree
+		// Sort the nodes in ascending order to begin constructing the tree
 		Collections.sort(nodes);
-		//Creating the HuffMan Tree
-		
-		//As long as we haven't created a root keep going
-		while(nodes.size() != 1) {
-			TreeNode combined  = new TreeNode(nodes.remove(0) , nodes.remove(0));
-			//finds the position at which the node shall be inserted to keep the order
+
+		// Creating the HuffMan Tree
+		// As long as we haven't created a root keep going
+		int depth = 0;
+		while (nodes.size() != 1) {
+			TreeNode combined = new TreeNode(nodes.remove(0), nodes.remove(0));
+			// finds the position at which the node shall be inserted to keep the order
 			int i = 0;
-			while(i < nodes.size()) {
-				if(nodes.get(i).compareTo(combined) != -1) {
+			depth++;
+			while (i < nodes.size()) {
+				if (nodes.get(i).compareTo(combined) != -1) {
 					break;
 				}
 				i++;
@@ -34,71 +51,97 @@ public class Compression {
 			nodes.add(i, combined);
 		}
 		root = nodes.get(0);
-		System.out.print(root);
-		
-		
+		fillReplacementTable(root , replacementTable , "");
+		// testing
+//		for(int i = 0 ; i < NUMBER_CHAR ; i++) {
+//			if(replacementTable[i] != null)
+//			System.out.println((char)i + " : " + replacementTable[i]);
+//		}
+//		System.out.println("Depth = " + depth);
+//		root.print();
+
 	}
+
 	// used for testing
 	public static void main(String[] args) {
-		String a = "<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>";
+//		String a = "<breakfast_menu><food><name>Belgian Waffles</name><price>$5.95</price><description>Two of our famous Belgian Waffles with plenty of real maple syrup</description><calories>650</calories></food><food><name>Strawberry Belgian Waffles</name><price>$7.95</price><description>Light Belgian waffles covered with strawberries and whipped cream</description><calories>900</calories></food><food><name>Berry-Berry Belgian Waffles</name><price>$8.95</price><description>Belgian waffles covered with assorted fresh berries and whipped cream</description><calories>900</calories></food><food><name>French Toast</name><price>$4.50</price><description>Thick slices made from our homemade sourdough bread</description><calories>600</calories></food><food><name>Homestyle Breakfast</name><price>$6.95</price><description>Two eggs, bacon or sausage, toast, and our ever-popular hash browns</description><calories>950</calories></food></breakfast_menu>";
+		String a = "AAAABBBBBCD";
 		encode(a);
 		System.out.println(a.length());
-			
-			
+
 	}
 
 }
 
-
-class TreeNode implements Comparable<TreeNode>{
+class TreeNode implements Comparable<TreeNode> {
 	protected int frequency;
 	protected TreeNode leftChild;
 	protected TreeNode rightChild;
 
-	//Creates a node from the combined frequencies of its children
-	public TreeNode(TreeNode leftChild , TreeNode rightChild){
+	// Creates a node from the combined frequencies of its children
+	public TreeNode(TreeNode leftChild, TreeNode rightChild) {
 		frequency = leftChild.frequency + rightChild.frequency;
 		this.leftChild = leftChild;
 		this.rightChild = rightChild;
 	}
-	// will be used in the child class leaf
+
+	// this will be used in the child class leaf
 	public TreeNode(int freq) {
 		frequency = freq;
 		leftChild = null;
 		rightChild = null;
-	}	
+	}
+
+	// in-order tree traversal
+	private void print(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		print(root.leftChild);
+		System.out.println(root);
+		print(root.rightChild);
+	}
+
+	public void print() {
+		print(this);
+	}
+
 	// this will be used later for sorting
 	@Override
 	public int compareTo(TreeNode nodeToCompare) {
-		if(frequency == nodeToCompare.frequency) {
+		if (frequency == nodeToCompare.frequency) {
 			return 0;
-		}
-		else if(frequency > nodeToCompare.frequency) {
+		} else if (frequency > nodeToCompare.frequency) {
 			return 1;
-		}
-		else {
+		} else {
 			return -1;
 		}
 	}
+
 	@Override
 	public String toString() {
 		return "" + frequency;
 	}
 }
 
-class Leaf extends TreeNode{
+class Leaf extends TreeNode {
 	private char character;
-	
+
 	/**
-	 *  Constructor creates a HuffMan tree leaf node
+	 * Constructor creates a HuffMan tree leaf node
+	 * 
 	 * @param character the character to be added
 	 * @param frequency the number of occurrences
-	*/
-	public Leaf(char character , int frequency){
+	 */
+	public Leaf(char character, int frequency) {
 		super(frequency);
 		this.character = character;
 	}
 	
+	public char getCharacter() {
+		return character;
+	}
+
 	@Override
 	public String toString() {
 		return character + " : " + frequency;
