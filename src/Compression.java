dc206febd7de7fce8000e298ff3d00bffcc63ed2
Amr Mohsen <amrmohsen2000@hotmail.com>
2021-12-08 21:28:46 +0200

    Created the tree data structure

--- src/Compression.java
+++ src/Compression.java
@@ -1,8 +1,56 @@
-
+import java.util.HashMap;
 public class Compression {
-
-	public Compression() {
-		// TODO Auto-generated constructor stub
+	
+	public static void main(String[] args) {
+			
+			
+			
+			
 	}
 
+}
+
+
+class TreeNode implements Comparable<TreeNode>{
+	protected int frequency;
+	protected TreeNode leftChild;
+	protected TreeNode rightChild;
+
+	//Creates a node from the combined frequencies of its children
+	public TreeNode(TreeNode leftChild , TreeNode rightChild){
+		frequency = leftChild.frequency + rightChild.frequency;
+		this.leftChild = leftChild;
+		this.rightChild = rightChild;
+	}
+	
+	// will be used in the child class leaf
+	public TreeNode(int freq) {
+		frequency = freq;
+		leftChild = null;
+		rightChild = null;
+	}
+		
+	// this will be used later for sorting
+	@Override
+	public int compareTo(TreeNode nodeToCompare) {
+		if(frequency == nodeToCompare.frequency) {
+			return 0;
+		}
+		else if(frequency > nodeToCompare.frequency) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+}
+
+class Leaf extends TreeNode{
+	private char character;
+	
+	//Creates a leaf node from a character and its frequency
+	public Leaf(char character , int frequency){
+		super(frequency);
+		this.character = character;
+	}
 }
