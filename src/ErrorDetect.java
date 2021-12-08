5c784a6f875e4a4cb78a85a2abf9d23d922a8c35
amr mahmoud <amrmah785@gmail.com>
2021-12-08 19:05:14 +0200

    Code for detecting Invalid Comments and detection as well as correction of missing opening or closing tags

--- src/ErrorDetect.java
+++ src/ErrorDetect.java
@@ -1,73 +1,191 @@
 import java.util.Stack;
+import java.util.*;
 public class ErrorDetect {
     
         static Stack<String> s = new Stack<String>();
+    static StringBuffer newXML;
+    static Vector<Integer> errorIndecies = new Vector<Integer>(1);
+
+    /*
+        static int incrementIndex(int index, String str){
         
-	static String getTag(String str, int index){
-            int j =0;
-            char tag[] = new char[15];                                      //3ayz at2ked mn length el tag
-            for(; !Character.isWhitespace(str.charAt(index)) && index != str.length() && str.charAt(index) != '>'; index++){
-                        tag[j] = str.charAt(index);
-                        j++;
-                    }
-        
+            for(int i = 0;  i<= str.length();i++)
+            index++;
+            return index;
+        }
+     */
+    static StringBuffer removeSpace(StringBuffer str) {
+        StringBuffer formattedXML = new StringBuffer(str);
+
+        for (int i = 1; i < formattedXML.length() - 1; i++) {
+            //32 is ascii code for blank space and 10 is ascii code for new lines
+            if (formattedXML.charAt(i) == 32 && formattedXML.charAt(i - 1) == 10 || formattedXML.charAt(i) == 32 && formattedXML.charAt(i - 1) == 32) {
+
+                formattedXML.deleteCharAt(i);
+                i--;
+
+            }
+
+        }
+
+        return formattedXML;
+    }
+
+    static void insertTag(StringBuffer str, int index, String ins) {
+
+        newXML = new StringBuffer(str.insert(index, ins));
+
+    }
+
+    static String getTag(StringBuffer str, int index) {
+        int j = 0;
+        char tag[] = new char[15];                                      //3ayz at2ked mn length el tag
+        for (; !Character.isWhitespace(str.charAt(index)) && index != str.length() && str.charAt(index) != '>'; index++) {
+            tag[j] = str.charAt(index);
+            j++;
+        }
+
         String tagStr = new String(tag);
         return tagStr;
-        
-        }
-        
-        
-        static Boolean error(String str){
-                
-                int index = 0;
-                
-                for(; index < str.length(); index++){
-                
-                if(str.charAt(index) == '<'){
-                    
-               
-                    
-                                                 
-                    index++;                   //momken a3mlha b i = index +1  badal m a3ml fe index 3la tool
-                    if(str.charAt(index) == '?'){
-                    
-                    for(; str.charAt(index) != '\n';index++);
-                    
+
+    }
+
+    static Boolean error(StringBuffer str) {
+
+        newXML = new StringBuffer(str);
+
+        Boolean tagValue = false;
+        int index = 0;
+        int newXMLindex = 0;
+        int count = 0;
+
+        for (; index < str.length(); index++, newXMLindex++) {
+            if (str.charAt(index) == 10) {
+                count++;
+            }
+            if (str.charAt(index) == '<') {
+
+                newXMLindex++;
+                index++;                   //momken a3mlha b i = index +1  badal m a3ml fe index 3la tool
+
+                if (str.charAt(index) == '?') {
+
+                    for (; str.charAt(index) != '\n'; index++, newXMLindex++);
+
                     continue;
-                    }
-                    
-                    if(str.charAt(index) != '/'){
-                        
-                    
-                    String tagStr = getTag(str,index);
-                    s.push(tagStr);
-                        System.out.println(s);
-                }
-                    if(str.charAt(index) == '/'){
-                        index++;
-                        String closingTag = getTag(str, index);
-                        if( s.empty() || !s.peek().equals( closingTag)){
-                            System.out.println("Unmatched tag");
-                            return false;
+                } else if (str.charAt(index) == '!' && str.charAt(index + 1) == '-' && str.charAt(index + 2) == '-') {
+                    int occurence = 0;
+                    for (int i = 0; str.charAt(index) != '>'; index++, newXMLindex++, i++) {
+                        if (i > 2) {
+                            if (str.charAt(index) == '-' && str.charAt(index - 1) == '-') {
+                                occurence++;
+                                if (occurence > 1) {
+                                    System.out.println("Comment format is Incorrect at line: " + count + 1);
+                                    errorIndecies.add(count + 1);
+                                }
+                            }
+
                         }
-                        s.pop();
-                        System.out.println(s);
-                        
-                        
-                    
+
                     }
+                    if (str.charAt(index - 1) != '-' || str.charAt(index - 2) != '-') {
+                        System.out.println("Comment format is Incorrect at line: " + count + 1);
+                        errorIndecies.add(count + 1);
+                    }
+
+                } else if (str.charAt(index) != '/' /*&& !Character.isWhitespace(str.charAt(index))*/) {
+                   
+
+                    String tagStr = getTag(str, index);
+                    //el function deh msh mzbota 3amlha 3shan case en el root node bta3 child node mt2flsh bs el child et2fl
+                    if (!s.empty() && tagStr.equals(s.peek())) {
+                        errorIndecies.add(count);
+                        int j = newXMLindex;
+                        String unmatchingTag = new String("</" + s.peek() + ">");
+                        System.out.println("Closing tag not found \n" + unmatchingTag + "\n");
+                        
+                        for (; newXML.charAt(j) != 10; j--);
+
+                        insertTag(newXML, j, unmatchingTag);
+                        s.pop();
+                        for (int i = 0; i < unmatchingTag.length(); newXMLindex++, i++);
+                    }
+                    s.push(tagStr);
+                    System.out.println(s);
+                } else if (str.charAt(index) == '/') {
+                    index++;
+                    newXMLindex++;
+                    String closingTag = getTag(str, index);
+                    //code for correcting the error in case of a missing openning tag
+
+                    if (!s.empty()  && !s.peek().equals(closingTag)) {
+                        errorIndecies.add(count + 1);
+                        System.out.println("Unmatched opening tag or missing openning tag");
+                        int j = newXMLindex;
+
+                        for (; newXML.charAt(j) != 10; j--);
+                        j++;
+                        
+                        String unmacthedTag = new String("<" + closingTag + ">");
+                        insertTag(newXML, j, unmacthedTag);
+
+                        for (int i = 0; i < unmacthedTag.length(); i++, newXMLindex++);
+
+                        tagValue = false;
+                        continue;
+                    }
+
+                    if (!s.empty()) {
+                        s.pop();
+                    } else {
+                        String rootTag = new String("<" + closingTag + ">");
+                        insertTag(newXML, 0, rootTag);
+                        for (int i = 0; i < rootTag.length(); i++, newXMLindex++);
+
+                        errorIndecies.add(0);
+                    }
+                    tagValue = false;
+                    System.out.println(s);
+
                 }
-                continue;
-        
-                }
-        
-        
-        
-        if(s.empty())
-            System.out.println("XML file is without errors");
-        else
-                System.out.println("XML file contains errors");
-        return s.empty();
+            } else if (Character.isLetterOrDigit(str.charAt(index)) && ( str.charAt(index - 1) == '>' || str.charAt(index-2)=='>')) {
+
+                tagValue = true;
+
+                for (; str.charAt(index) != 10 && str.charAt(index) != '<'; index++, newXMLindex++);
+
+                index--;
+                newXMLindex--;
+
+            }
+             //function for correcting in case there is no closing tag for a certain node
+            if (str.charAt(index) == 10 && tagValue) {
+
+                errorIndecies.add(count);
+                String unmatchingTag = new String("</" + s.peek() + ">");
+                System.out.println("Closing tag not found \n" + unmatchingTag + "\n");
+                //3ayz ala2y 7al fe index - 4 w index - 2 w kdh
+                insertTag(newXML, newXMLindex, unmatchingTag);
+
+                s.pop();
+                tagValue = false;
+
+                for (int i = 0; i < unmatchingTag.length(); newXMLindex++, i++);
+
+            }
+            continue;
+
         }
+
+        if (s.empty()) {
+            System.out.println("XML file is without errors");
+        } else {
+            insertTag(newXML, newXMLindex, "</" + s.peek() + ">");
+            errorIndecies.add(count + 1);
+            s.pop();
+            System.out.println("XML file contains errors");
+        }
+        return s.empty();
+    }
 
 }
