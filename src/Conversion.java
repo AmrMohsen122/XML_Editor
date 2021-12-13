117ae434ab01db8e83dc3e20f002b5991933e27f
amr abd elaaal <amrmohamed362363@gmail.com>
2021-12-13 18:46:27 +0200

    editing

--- src/Conversion.java
+++ src/Conversion.java
@@ -7,35 +7,45 @@ public class Conversion {
 
                 
 	}
-	static void parsing_xml(String sa,ArrayList<String> string) {
-		char space=32;
-		for (int i = 0; i < sa.length(); i++) {
-			if (sa.charAt(i)==space||sa.charAt(i)=='\r'||sa.charAt(i)=='\n')continue;
-			
-			
-			else if (sa.charAt(i)=='<') {
-				
-				int j=i;
-				
-				while ((sa.charAt(i)!='>')) {
-					i++;
-				}
-				//if (sa.charAt(j+1)=='/')continue;
-				string.add(sa.substring(j,i+1));
-				
-				
-			}
-			else {
-				int k=i;
-				while((sa.charAt(i)!='<')) {
-					
-					if (sa.charAt(i)=='\r'||sa.charAt(i)=='\n')break;
+	static public void parsing_xml(String sa,ArrayList<String> string) {
+	char space=32;
+	for (int i = 0; i < sa.length(); i++) {
+		if (sa.charAt(i)=='<'&&sa.charAt(i+1)=='!') {
+			while(sa.charAt(i)!='>') {
 				i++;}
-				string.add(sa.substring(k,i));
-				--i;
-			}
+			i++;
+			
 		}
+		if (sa.charAt(i)=='<'&&sa.charAt(i+1)=='?') {
+			while(sa.charAt(i)!='>') {
+				i++;}
+			i++;
+			
+		}
+		if (sa.charAt(i)==space||sa.charAt(i)=='\r'||sa.charAt(i)=='\n')continue;
 		
+		
+		else if (sa.charAt(i)=='<') {
+			
+			int j=i;
+			
+			while ((sa.charAt(i)!='>')) {
+				i++;
+			}
+			//if (sa.charAt(j+1)=='/')continue;
+			string.add(sa.substring(j,i+1));
+			
+			
+		}
+		else {
+			int k=i;
+			while((sa.charAt(i)!='<')) {
+				
+				if (sa.charAt(i)=='\r'||sa.charAt(i)=='\n')break;
+			i++;}
+			string.add(sa.substring(k,i));
+			--i;
+		}
+	}
 }
-
 }
