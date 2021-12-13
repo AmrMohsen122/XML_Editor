a101ce00968c73d5b3a4c7936f60d3820f4ca48b
Mo'men Waleed <momen.waleed306@gmail.com>
2021-12-14 00:07:33 +0400

    Error_Resolved

--- src/Format.java
+++ src/Format.java
@@ -5,11 +5,11 @@ public class Format {
 		int level = 0;
 		List<String> textList = new ArrayList<String>();
 		int i = 0;
-		while (i < unformatted.length()-1) {
+		while (i > -1 && i < unformatted.length()-1) {
 			if (unformatted.charAt(i) == '<' && unformatted.charAt(i+1) == '?') {
 				int closingIndex = unformatted.indexOf('>', i);
 				String tagName = unformatted.substring(i+2, closingIndex);
-				textList.add(" ".repeat(level * 2) + "<?" + tagName + ">");
+				textList.add("<?" + tagName + ">");
 				i = closingIndex + 1;
 				continue;
 			}
@@ -26,7 +26,7 @@ public class Format {
 				String tagName = unformatted.substring(i+2, closingIndex);
 				level--;
 				textList.add(" ".repeat(level * 2) + "</" + tagName + ">");
-				i = closingIndex;
+				i = unformatted.indexOf('<', closingIndex);
 				continue;
 			}
 			int nextTagStartIndex = unformatted.indexOf('<', i);
@@ -40,6 +40,15 @@ public class Format {
 				continue;
 			}
 			String text = unformatted.substring(i, nextTagStartIndex);
+			int characterIndex = 0;
+			while (text.charAt(0) == ' ' || text.charAt(0) == '\r' || text.charAt(0) == '\n') {
+				text = text.substring(1);
+			}
+			characterIndex = text.length() - 1;
+			while (text.charAt(characterIndex) == ' ' || text.charAt(characterIndex) == '\r' || text.charAt(characterIndex) == '\n') {
+				text = text.substring(0, characterIndex);
+				characterIndex = text.length() - 1;
+			}
 			textList.add(" ".repeat(level * 2) + text);
 			i = nextTagStartIndex;
 		}
